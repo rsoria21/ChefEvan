b4ff61759f8b3de007e30cff935ff494a0ac13a1
Ricardo Soria <ricardo@ricardos-mbp.attlocal.net>
2021-05-06 16:42:38 -0500

    Added extra detail

--- script.js
+++ script.js
@@ -24,21 +24,37 @@ function responsiveNav() {
 }
 // navbar end
 
-// review slider
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function carousel() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
     slideIndex = 1;
   }
-  x[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 3000);
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
-// review slider end
+
+// Skills carousel End
